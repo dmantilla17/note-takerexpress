c9eefc26c5b661770ccc393ea59d2718321f71e5
dmantilla17 <derekaraujo11@gmail.com>
2023-03-11 20:05:15 -0500

    express public and added port

---
+++ Develop/personel/server.js
@@ -0,0 +1,13 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+//INITIALZATION
+const app = express();
+const PORT = 3001;
+
+//MIDDLEWARE
+app.use(express.static("public"));
+//CLICK
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
