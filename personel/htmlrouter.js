094b1b8fb56658dc5e00cd263126c7834de0200d
dmantilla17 <derekaraujo11@gmail.com>
2023-04-06 11:47:37 -0400

    moved server.js

--- personel/htmlrouter.js
+++ personel/htmlrouter.js
@@ -3,12 +3,12 @@ const path = require("path");
 const router = require("express").Router();
 //ROUTES
 //Links our html to servers
-router.get("./public/index", (req, res) => {
+router.get("/", (req, res) => {
   console.log("Success");
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 router.get("./public/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, ""));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const path = require("path");
 const apiroutes = require("./apiroutes");
 const htmlroutes = require("./htmlrouter");
+const db = require('./db/db.json');
+
 //INITIALZATION
 const app = express();
 const PORT = 3001;
 
 //MIDDLEWARE
 app.use(express.static("public"));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 app.use(apiroutes);
 app.use(htmlroutes);
 //CLICK
